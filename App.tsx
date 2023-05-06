ed92a97130e6d15a3240013d6824a9f16670a750
DeerGram <92402622+DeerGram@users.noreply.github.com>
2023-05-05 22:59:59 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ThemeContextProvider from './contexts/ThemeContext';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import OrderDashboard from './components/OrderDashboard';
+import AnalyticsDashboard from './components/AnalyticsDashboard';
+import ToppingManagement from './components/ToppingManagement';
+import CustomerInterface from './components/CustomerInterface';
+import PieArtistAnalytics from './components/PieArtistAnalytics';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeContextProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Sidebar />
+          <Switch>
+            <Route path="/orders" component={OrderDashboard} />
+            <Route path="/analytics" component={AnalyticsDashboard} />
+            <Route path="/toppings" component={ToppingManagement} />
+            <Route path="/customer" component={CustomerInterface} />
+            <Route path="/pie-artist" component={PieArtistAnalytics} />
+          </Switch>
+        </div>
+      </Router>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
